1f879fb8b5ed3231f36ba6a8aee6d22924a91b42
Vasya Ichkoff <vasyaichkoff@gmail.com>
2019-08-12 22:24:14 +0300

    Start project

---
+++ mainwindow.h
@@ -0,0 +1,39 @@
+
+// Copyright (c) 2019 The Karbowanec developers
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// Code formatting based on CS106B Style
+
+#ifndef MAINWINDOW_H_INCLUDED
+#define MAINWINDOW_H_INCLUDED
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow {
+  Q_OBJECT
+
+  public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+  private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H_INCLUDED
